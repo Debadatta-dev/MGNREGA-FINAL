b18e5abe3eaadba85d43f943d255e97fdc89e664
Debadatta-dev <debadattam100@gamil.com>
2025-11-03 15:45:46 +0530

    m

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    host: true
-  }
 })
